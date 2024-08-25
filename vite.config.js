efa3e0fad440dfd8f0786fe74ed29f9e560a2dbd
Ira M <iraamane1403@gmail.com>
2024-08-25 19:47:51 +0530

    Changes In file

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-})
-
-// export default defineConfig({ 
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: () => {
-//           return {
-//             vendor: ['react', 'react-dom'],
-//           };
-//         },
-//       },
-//     },
-//     chunkSizeWarningLimit: 1600
-//   },
-//   plugins: [react()]
-// });
-
+  plugins: [
+    react({
+      build: {
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+            }
+          }
+        }
+      }
+    }) // added closing bracket
+  ] // added closing bracket
+}); // added closing bracket
 
